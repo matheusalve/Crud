5e2be6d123fc1399c7f0f902206be011b18ae616
Matheus <matheusalvescarvalho07@gmail.com>
2023-08-04 14:39:08 -0300

    Criação do modelo post

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -5,7 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Employee extends Model
-{
+class Employee extends Model {
     use HasFactory;
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'email',
+        'phone',
+        'post',
+        'avatar'
+    ];
 }
